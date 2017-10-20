3085350d6bf214b7d579019ab9e6f7ad56f7fb1c
Margaretha Juan <tulip_holy@yahoo.com>
2017-10-20 00:23:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,16 @@ function nowServing(currentQue){
 }
 
 function currentLine(line){
-  
+  var tempText = ""
+  if(line.length < 1){
+    tempText = "The line is currently empty."
+  }
+  else {
+    tempText = "The line is currently: "
+    for(let i=0; i < line.length-1; i++){
+      tempText = tempText + `${i+1}. ${line[i],}`
+    }
+    tempText = tempText + `${line.length}. ${line[i]}`
+  }
+  return tempText
 }
