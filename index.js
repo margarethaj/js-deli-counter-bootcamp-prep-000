4b41c4f6ece35559db7d8382edb295b9a1c5048b
Margaretha Juan <tulip_holy@yahoo.com>
2017-10-20 00:17:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ function takeANumber(currentQue, newName){
   currentQue.push(newName);
   return `Welcome, ${newName}. You are number ${currentQue.length} in line.`
 }
+
+function nowServing(currentQue){
+  var tempText = ""
+  if(currentQue.length < 1){
+    tempText = "There is nobody waiting to be served!"
+  }
+  else{
+    tempText = `Currently serving ${currentQue[0]}.`
+    currentQue.shifts();
+  }
+  return tempText;
+}
